705d2a48fd762120e481e9091394b4fa414d1a95
Binh Thuan <binhthuan3101@gmail.com>
2026-06-12 00:55:30 +0700

    Day13

---
+++ Day13/script.js
@@ -0,0 +1,22 @@
+// var user = "F8";
+// console.log(user);
+
+// const name = "Bình Thuận";
+// console.log(name);
+// let age = 20;
+// console.log(age);
+// age = 21;
+// console.log(age);
+// // tên biến
+// let money = 1000;
+// console.log(mone
+// if (true) {
+//     // block scope
+//     let netSalary = 500;
+// }
+// {
+//     var grossSalary = 1000;
+// }
+// console.log(grossSalary);
+var myclass = "K20";
+console.log(myclass);
