558f50ef9d9a290421286e42ff7e3256cc3d1778
Binh Thuan <binhthuan3101@gmail.com>
2026-09-07 19:55:02 +0700

    day 32.1

--- k20-day32-react-components-props/vite.config.js
+++ k20-day32-react-components-props/vite.config.js
@@ -1,9 +1,17 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import {resolve} from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      }
+    }
+  }
 });
